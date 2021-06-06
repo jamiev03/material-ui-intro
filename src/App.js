d7ee9c35eda903a56b7c20f6e1610651ee7ec0d6
Jamie <jamie03.v@gmail.com>
2021-06-06 12:19:24 -0400

    appbar, icon and text added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+// const people = [
+//   {
+//     name: 'James',
+//     age: 31,
+//   },
+//   {
+//     name: 'John',
+//     age: 45,
+//   },
+//   {
+//     name: 'Paul',
+//     age: 65,
+//   },
+//   {
+//     name: 'Ringo',
+//     age: 49,
+//   },
+//   {
+//     name: 'George',
+//     age: 34,
+//   }
+// ];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* sets base css */}
+    <CssBaseline/>
+    <AppBar position='relative'>
+      <Toolbar>
+        <CameraIcon />
+        <Typography variant='h6'>
+          Photo Album
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    </>
   );
 }
 
