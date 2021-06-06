2e165dfcac5606c4a0c9f21f38f0a4f8d15f39ff
Jamie <jamie03.v@gmail.com>
2021-06-06 15:13:33 -0400

    image container created, class styling used for button and header

--- src/App.js
+++ src/App.js
@@ -10,27 +10,20 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6)
-  }
-}))
+import useStyles from './Styles';
 
 function App() {
   const classes = useStyles();
-  
+
   return (
     <>
       {/* sets base css */}
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <CameraIcon />
+          <CameraIcon className={classes.icon}/>
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
@@ -41,7 +34,7 @@ function App() {
             <Typography variant='h5' align='center' color='textSecondary' paragraph>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga laborum illum molestias sunt expedita dolores, deleniti tempore quia consequuntur sit culpa accusamus laudantium repellendus quos perspiciatis magnam ullam veniam asperiores.
             </Typography>
-            <div>
+            <div className={classes.buttons}>
               <Grid container spacing={3} justify='center'>
                 <Grid item>
                      <Button variant='contained' color='primary'>See Photos</Button>
@@ -53,6 +46,19 @@ function App() {
             </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardMedia 
+                className={classes.cardMedia}
+                image='https://source.unsplash.com/random'
+                />
+              </Card>
+            </Grid>
+
+          </Grid>
+        </Container>
       </main>
     </>
   );
